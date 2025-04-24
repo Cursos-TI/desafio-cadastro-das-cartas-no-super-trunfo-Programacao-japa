4d1affa6b7eb98b3987f45a7cf857ba626b6823b
Programacao-japa <adriano.lera@hotmail.com>
2025-04-24 19:57:51 +0000

    editei carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 
-int main (){
-    printf("Jogo super trunfo!\n");
-    printf("Novo commit\n");
-    return 0;
-}
-
-#include <stdio.h>
-#include <string.h>
-
-struct CartasSuperTrunfo {
+//Declaração da struct antes do main
+struct Carta {
+    char estado;
+    char codigo [4];
+    char nomeCidade [50];
     int populacao;
     float area;
     float pib;
-    int numerodepontosturisticos;
+    int pontosturisticos;
 };
+
+int main() {
+    struct Carta carta1, carta2;
+
+    // Leitura dos dados da carta 1
+    printf("Digite os dados da carta 1:\n");
+    printf("Estado: ");
+    scanf("%c", &carta1.estado);
+    printf("Código: ");
+    scanf("%s", carta1.codigo);
+    printf("nome da cidade:");
+    scanf("%[^\n]",carta1.nomeCidade);
+    printf("População: ");
+    scanf("%d", &carta1.populacao);
+    printf("Área: ");
+    scanf("%f", &carta1.area);
+    printf("PIB: ");
+    scanf("%", &carta1.pib);
+    printf("Número de pontos Turisticos: ");
+    scanf("%d", &carta1.pontosturisticos);
+    
+}
