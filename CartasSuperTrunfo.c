89ec28fa991aaae6f1b83e6fe7a60540cb1e6b55
Programacao-japa <adriano.lera@hotmail.com>
2025-04-24 18:39:31 +0000

    Criei cadrastro das CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,4 +3,15 @@
 int main (){
     printf("Jogo super trunfo!\n");
     printf("Novo commit\n");
+    return 0;
 }
+
+#include <stdio.h>
+#include <string.h>
+
+struct CartasSuperTrunfo {
+    int populacao;
+    float area;
+    float pib;
+    int numerodepontosturisticos;
+};
